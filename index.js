52026553c86da5023d63ae58adab5be0dca7aca5
Birendra <divyabiru27@gmail.com>
2023-07-09 09:47:12 +0530

    work on github

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ function closemenu(){
   sidemenu.style.right = "-200px";
 }
 
-// function open_in_newtab(){
-//   window.open("https://drive.google.com/file/d/1ZvaB_-bSCtWWAGExGhZ51e9v1n0b9ixG/view?usp=sharing");
-// }
 
-// let resumebtn1 = document.getElementById("resume-button-1").addEventListener("click",()=>{
-//   window.location.assign("https://drive.google.com/file/d/1ZvaB_-bSCtWWAGExGhZ51e9v1n0b9ixG/view?usp=sharing")
-// })
 function homePage(){
   window.location.href='#home'
 }
